262e79d1f3b5f029cf6187d9551f213d1608f89e
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-17 14:30:33 +0900

    feat: comment controller 구현

---
+++ src/main/java/com/repick/comment/controller/CommentController.java
@@ -0,0 +1,47 @@
+package com.repick.comment.controller;
+
+import com.repick.comment.domain.Comment;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    @GetMapping
+    public List<Comment> getComments() {
+        return service.getAllComments();
+    }
+
+    @GetMapping("/{id}")
+    public Comment getComment(@PathVariable Long id) {
+        return service.getCommentById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(@RequestBody CreateCommentRequest request) {
+        return service.createComment(request);
+    }
+
+    @PutMapping("/{id}")
+    public Comment updateComment(@RequestBody UpdateCommentRequest request) {
+        return service.updateComment(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable Long id) {
+        service.deleteComment(id);
+    }
+}
