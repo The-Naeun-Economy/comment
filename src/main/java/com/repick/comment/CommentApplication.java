14b93080b89a4418738a1b75d513dd6fd3a75ef5
llcodingll <ll.o.llwhkr@gmail.com>
2024-12-03 14:45:20 +0900

    feat: EnableJpaAuditing기능 활성화

--- src/main/java/com/repick/comment/CommentApplication.java
+++ src/main/java/com/repick/comment/CommentApplication.java
@@ -3,10 +3,10 @@ package com.repick.comment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication
 public class CommentApplication {
 
